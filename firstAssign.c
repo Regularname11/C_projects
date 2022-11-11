4d938e30db16721f5616c099b820a236d211d885
Taha enn <tahaennabili@gmail.com>
2022-11-11 15:15:29 +0200

    Update firstAssign.c

--- firstAssign.c
+++ firstAssign.c
@@ -9,9 +9,9 @@ int main() {
     scanf("%lf", &number);
     printf("Double number you entered is: %lf", number);
 
-    printf("\n Enter a name: ");
+   /* printf("\n Enter a name: ");
     scanf("%c", &alphabet);
-    printf("\n the name you entered is: %c", alphabet);
+    printf("\n the name you entered is: %c", alphabet); */
 
 
     return 0;
