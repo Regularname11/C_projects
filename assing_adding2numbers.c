60a1d92800c682c5352b7f53a33808d2836ecd04
Taha enn <tahaennabili@gmail.com>
2022-11-11 15:56:06 +0200

    Create assing_adding2numbers.c
    reading user inputs and giving output.

---
+++ assing_adding2numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    double number1;
+    double number2;
+
+    printf("Enter your first number: ");
+    scanf("lf", &number1);
+
+    printf("Enter your second number: ");
+    scanf("lf", &number2);
+
+    printf("The sum of the two numbers is %f%f: ", number1 + number2);
+
+    return 0;
+}
