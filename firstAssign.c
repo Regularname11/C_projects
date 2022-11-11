966f42e3c86d8031e41cb66c553c8dfcacb978f4
Taha enn <tahaennabili@gmail.com>
2022-11-11 15:23:50 +0200

    Update firstAssign.c

--- firstAssign.c
+++ firstAssign.c
@@ -3,11 +3,15 @@
 
 int main() {
     double number;
+    int age;
     char alphabet;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    printf("You are %d YO : ", age);
 
-    printf("Enter double input: ");
-    scanf("%lf", &number);/*&number (called pointer)*/
-    printf("Double number you entered is: %lf", number);
+   /* printf("Enter double input: ");
+    scanf("%lf", &number);/*&number (called pointer)
+    printf("Double number you entered is: %lf", number);*/
 
    /* printf("\n Enter a name: ");
     scanf("%c", &alphabet);
