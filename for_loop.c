dbabb8b4b31078990ab194b76aa482c976ae62db
Taha enn <tahaennabili@gmail.com>
2022-12-05 17:06:26 +0200

    Update for_loop.c

--- for_loop.c
+++ for_loop.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main() {
+    int numbers[] = {1,16,123,458,12,10};
     int num;
 
-    for ( num = 0; num <= 5; num++)
+    for ( num = 0; num < 6; num++)
     {
-        printf("%d\n", num);
+        printf("%d\n", numbers[num]);
     }
     
     return 0;
